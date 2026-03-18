b371717af856c11b3e46833ec4845d148036c868
aaronvladimir <tokovalentine01@gmail.com>
2026-03-18 16:59:35 +0700

    init:Opening for user

---
+++ belajar github.py
@@ -0,0 +1,11 @@
+print(
+    "Halo kawan kawan, ini adalah project python pertama saya " \
+    "yang saya akan masukan github menggunakan terminal, belajar github..."
+    "Projectnya adalah membuat kalkulator " \
+    "sederhana dengan python, semoga berhasil" \
+)
+
+angka1=int(input("Masukan angka pertama: "))
+angka2=int(input("Masukan angka kedua: "))
+
+print("Terimakasih Sudah Input"))
